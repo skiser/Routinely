bed348803200eebfefb5b1056a25aa11ac328677
Chase Teichmann <cteichmann@ycp.edu>
2019-10-09 12:46:24 -0400

    Large Push-may have conflicts

--- Routinely/Navigation.js
+++ Routinely/Navigation.js
@@ -3,6 +3,7 @@ import LoginScreen from './app/components/login';
 import {createStackNavigator} from 'react-navigation-stack';
 import CalendarScreen from './app/components/calendar';
 import AlarmScreen from './app/components/alarm';
+import AlarmRingingScreen from './app/components/alarmRinging';
 
 const AuthStack = createStackNavigator(
     {
@@ -17,10 +18,12 @@ const AuthStack = createStackNavigator(
 const AppStack = createStackNavigator(
 {
     Alarm: AlarmScreen,
+    AlarmRinging: AlarmRingingScreen,
     Calendar: CalendarScreen,
 },
 {
     initialRouteName: "Alarm",
+    initialRouteName: "AlarmRinging",
     initialRouteName: "Calendar",
     //aheaderMode: "none"
 }
--- Routinely/app/components/alarm.js
+++ Routinely/app/components/alarm.js
@@ -6,11 +6,13 @@ import SnoozeDuration from './alarm_components/SnoozeDuration';
 import TimePicker from './alarm_components/TimePicker';
 import {Divider} from 'react-native-elements';
 import ColorPicker from './alarm_components/ColorPicker';
+import TitleInput from './alarm_components/TitleInput';
 
 class AlarmScreen extends Component {
   render() {
     return (
       <View>
+        <TitleInput/>
         <TimePicker/>
         <RepeatDiv/>
         <Divider/>
@@ -21,21 +23,30 @@ class AlarmScreen extends Component {
         <ColorPicker/>
         <Divider/>
         <Button
+          buttonStyle = {styles.bottomLinks}
           title="Calendar"
           onPress={() => this.props.navigation.navigate('Calendar')}
         />
          <Button
+          buttonStyle = {styles.bottomLinks}
           title="Logout"
           onPress={() => this.props.navigation.navigate('Login')}
+        />
+        <Button
+          buttonStyle = {styles.bottomLinks}
+          title="Alarm Ringing"
+          onPress={() => this.props.navigation.navigate('AlarmRinging')}
         />
       </View>
 
     );
   }
-} 
-
+}
 const styles = StyleSheet.create({
- 
+    bottomLinks:{
+      flexDirection: 'row',
+      color: 'red',
+    }
 });
 
 export default AlarmScreen;
