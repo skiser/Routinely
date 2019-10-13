e2d2a4f8a01a5aa5d74df9ad7de5b2538c63c2d0
Chase Teichmann <cteichmann@ycp.edu>
2019-10-12 20:42:13 -0400

    ESLint issues on navigation

--- Routinely/app/navigation.js
+++ Routinely/app/navigation.js
@@ -1,4 +1,4 @@
-import {createAppContainer, createSwitchNavigator } from 'react-navigation';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import LoginScreen from './components/login';
 import {createStackNavigator} from 'react-navigation-stack';
 import CalendarScreen from './components/calendar';
@@ -7,40 +7,38 @@ import AlarmRingingScreen from './components/alarmRinging';
 import EventScreen from './components/event';
 
 const AuthStack = createStackNavigator(
-    {
-      Login: LoginScreen,
-    },
-    {
-      headerMode: "none",
-      initialRouteName: "Login"
-    }
-  ); 
+  {
+    Login: LoginScreen,
+  },
+  {
+    headerMode: 'none',
+    initialRouteName: 'Login',
+  },
+);
 
 const AppStack = createStackNavigator(
-{
+  {
     Alarm: AlarmScreen,
     AlarmRinging: AlarmRingingScreen,
     Calendar: CalendarScreen,
     Event: EventScreen,
-},
-{
-    initialRouteName: "Alarm",
-    initialRouteName: "AlarmRinging",
-    initialRouteName: "Calendar",
-    //aheaderMode: "none"
-}
+  },
+  {
+    initialRouteName: 'Alarm',
+    initialRouteName: 'AlarmRinging',
+    initialRouteName: 'Calendar',
+  },
 );
 
-
 export default createAppContainer(
-    createSwitchNavigator(
-      {
-        App: AppStack,
-        Auth: AuthStack,
-        //Calendar: CalendarScreen,
-      },
-      {
-        initialRouteName: "Auth"
-      }
-    )
-  );
+  createSwitchNavigator(
+    {
+      App: AppStack,
+      Auth: AuthStack,
+      //Calendar: CalendarScreen,
+    },
+    {
+      initialRouteName: 'Auth',
+    },
+  ),
+);
