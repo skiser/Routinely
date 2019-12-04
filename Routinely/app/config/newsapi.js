3dae5575890d77dd62ad75d26c5b1cb15b472bcc
Bryan Soto <bsoto@ycp.edu>
2019-12-04 12:02:08 -0500

    working news

---
+++ Routinely/app/config/newsapi.js
@@ -0,0 +1,4 @@
+export const api_url = 'https://newsapi.org/v2/top-headlines';
+export const country_code = 'us';
+export const category = 'general';
+export const api_key = '20251f738c79400dab399bb73d50bfc3';
