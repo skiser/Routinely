02eae434bc5415e3f35f6de58050f00ef0de322b
Philip Norris <pnorris@ycp.edu>
2019-12-09 21:29:49 -0500

    Modified the event notifications.

--- Routinely/app/components/event_components/EventNotifManager.m
+++ Routinely/app/components/event_components/EventNotifManager.m
@@ -7,9 +7,37 @@
 // To export a module named EventNotifManager
 RCT_EXPORT_MODULE();
 
-RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+RCT_EXPORT_METHOD(addEvent:(NSString *)id, (NSString *)title, (NSString *)notes, (NSDate *)chosenDate, (Boolean *)Sun, Boolean *)Mon, (Boolean *)Mon, (Boolean *)Tue, (Boolean *)Wed, (Boolean *)Thu, (Boolean *)Fri, (Boolean *)Sat)
 {
-  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+    NSString *location = [RCTConvert NSString:details[@"location"]];
+    // NSDate *time = [RCTConvert NSDate:chosenDate[@"time"]];
+    NSDateComponents *triggerDate = [[NSCalendar currentCalendar]   
+                components:NSCalendarUnitYear +
+                NSCalendarUnitMonth + NSCalendarUnitDay +
+                NSCalendarUnitHour + NSCalendarUnitMinute +
+                NSCalendarUnitSecond fromDate:chosenDate];
+    UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger triggerWithDateMatchingComponents:triggerDate
+                                            repeats:NO];
+    NSString *identifier = @"UYLLocalNotification";
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier
+                                    content:content trigger:trigger]
+
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        if (error != nil) {
+            NSLog(@"Something went wrong: %@",error);
+        }
+    }];
+    UNNotificationAction *snoozeAction = [UNNotificationAction actionWithIdentifier:@"Snooze"
+    title:@"Snooze" options:UNNotificationActionOptionNone];
+    UNNotificationAction *deleteAction = [UNNotificationAction actionWithIdentifier:@"Delete"
+    title:@"Delete" options:UNNotificationActionOptionDestructive];
+    UNNotificationCategory *category = [UNNotificationCategory categoryWithIdentifier:@"UYLReminderCategory"
+    actions:@[snoozeAction,deleteAction] intentIdentifiers:@[]
+    options:UNNotificationCategoryOptionNone];
+    NSSet *categories = [NSSet setWithObject:category];
+    [center setNotificationCategories:categories];
+    content.categoryIdentifier = @"UYLReminderCategory";
 }
+
 
 @end
